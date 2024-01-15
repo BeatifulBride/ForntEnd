340295adcab49d63149dbf81e085de4f63b7def4
jiyoubin <hbsjyb@gmail.com>
2024-01-15 17:38:23 +0900

    0113 업체페이지 css 완료 및 예외처리, 코드 정리

--- react/src/pages/company/CompanyDressList.js
+++ react/src/pages/company/CompanyDressList.js
@@ -12,6 +12,9 @@ import {
 } from '../../apis/ProductAPICalls'
 import axios from "axios";
 import CompanyMypageCSS from "./CompanyMypage.module.css";
+import {Icon} from "@iconify/react";
+import Footer from "../../components/common/Footer";
+import Swal from "sweetalert2";
 
 function  CompanyDressList() {
         const dressInfo = useSelector(state => state.productReducer);
@@ -25,6 +28,66 @@ function  CompanyDressList() {
         const [hasMore, setHasMore] = useState(true);
         const [loading, setLoading] = useState(false);
         const accessToken = sessionStorage.getItem("accessToken");
+
+    const showAlert = () => {
+        if (!accessToken) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "유효한 토큰값이 없습니다.",
+                showConfirmButton: false,
+                timer: 3000,
+            }).then(() => {
+                // 3초 후에 실행
+                setTimeout(() => {
+                    window.location.href = '/login'; // '/login'은 로그인 페이지 경로에 따라 수정
+                });
+            });
+        } else {
+
+        }
+    };
+
+
+    useEffect(() => {
+        showAlert()
+    }, []);
+
+
+    const info = () => {
+        axios.post(
+            'http://1.214.19.22:6900/com/mypage',
+            {},
+            {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            }
+        ).then((response) => {
+            // HTTP 상태 코드가 200일 때 응답 데이터에서 정보를 추출합니다.
+            if (response.status === 200) {
+                const {
+                    companyName,
+
+                } = response.data;
+
+                // Setting company information
+                setCompanyName(companyName);
+
+
+            }
+
+        }).catch((error) => {
+            // 에러 핸들링
+            // alert('에러');
+        });
+    };
+
+    useEffect(() => {
+        info()
+    }, []);
 
     const list = () => {
         axios.post(
@@ -51,26 +114,13 @@ function  CompanyDressList() {
                     setDressName(dressName);
                     setMarkCount(markCount);
                     setDressInfoIndex(dressInfoIndex);
-
-
-                    // 필요한 경우, 추가적인 정보 로깅
-                    companyAllDressList.forEach((dress, i) => {
-                        console.log(`Dress ${i + 1} in companyAllDressList:`, {
-                            dressImagePath: dress.dressImagePath,
-                            dressName: dress.dressName,
-                            dressInfoIndex: dress.dressInfoIndex,
-                            markCount: dress.markCount
-                        });
-                    });
                 }
 
-                console.log("companyAllDressList:", companyAllDressList);
             }
 
         }).catch((error) => {
             // 에러 핸들링
-            console.error("An error occurred:", error);
-            alert('에러');
+            // alert('에러');
         });
     };
 
@@ -94,48 +144,107 @@ function  CompanyDressList() {
     const imageUrl = `http://1.214.19.22:6900/${dressImagePath}`;
 
     const handleDelete = (dressIndex) => {
-        const dressToDelete = currentItems[dressIndex];
-        axios.delete(
-            `http://1.214.19.22:6900/dress/del/${dressToDelete.dressInfoIndex}`,
-            {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`,
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                }
+        Swal.fire({
+            title: "삭제하시겠습니까?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes!",
+            cancelButtonText: "No!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // SweetAlert2에서 확인 버튼을 눌렀을 때 삭제 로직 실행
+                const dressToDelete = currentItems[dressIndex];
+                axios.delete(
+                    `http://1.214.19.22:6900/dress/del/${dressToDelete.dressInfoIndex}`,
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${accessToken}`,
+                            "Content-Type": "application/json",
+                            "Accept": "application/json"
+                        }
+                    }
+                ).then(response => {
+                    if (response.status === 200) {
+                        // 드레스 삭제 후 상태 업데이트
+                        const updatedItems = currentItems.filter((item, index) => index !== dressIndex);
+                        setCurrentItems(updatedItems);
+                        Swal.fire(
+                            "삭제에 성공하였습니다.",
+                            "success"
+                        );
+                    }
+                }).catch(error => {
+                    Swal.fire("에러", "삭제에 실패하였습니다.", "error");
+                });
+            } else {
+                // SweetAlert2에서 취소 버튼을 눌렀을 때의 동작
+                Swal.fire("취소되었습니다.", "Your imaginary file is safe :)", "info");
             }
-        ).then(response => {
-            if(response.status === 200) {
-                // 드레스 삭제 후 상태 업데이트
-                const updatedItems = currentItems.filter((item, index) => index !== dressIndex);
-                setCurrentItems(updatedItems);
-                console.log('삭제 성공 ', dressInfoIndex);
-            }
-        }).catch(error => {
-            console.error("An error occurred:", error);
-            alert('에러');
         });
+    };
+
+    const [companyName, setCompanyName] = useState('');
+
+    const handleCompanyNameChange = (e) => {
+        setCompanyName(e.target.value);
+    };
+
+    const handleLogout = () => {
+        Swal.fire({
+            title: "로그아웃 하시겠습니까?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes!",
+            cancelButtonText: "No!",
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        "메인화면으로 돌아갑니다.",
+                        "로그아웃에 성공하였습니다.",
+                        "success"
+                    );
+                    // 로그아웃 처리 및 기타 작업 수행
+                    sessionStorage.clear();
+                    window.location.href = '/'; // 메인 페이지로 이동
+                } else {
+                    Swal.fire("취소되었습니다.", "Your imaginary file is safe :)", "info");
+                }
+            });
     };
 
 
 
 
     return (
-        <div>
+        <div className={CompanyDressListCSS.background}>
+            {/*업체 환영*/}
+            <div className={CompanyDressListCSS.companyDiv}>
+                <input type='text' className={CompanyDressListCSS.name} value={companyName} onChange={handleCompanyNameChange}/> <span className={CompanyDressListCSS.a}>님 환영합니다.</span>
+                <button className={CompanyDressListCSS.logout} onClick={handleLogout}>로그아웃</button>
+                <Icon className={CompanyDressListCSS.icon_logout} icon="line-md:logout" />
+            </div>
+            <div className={CompanyDressListCSS.list}>
             {loading && <LoadingDots />}
             <InfiniteScroll
                 dataLength={currentItems.length}
                 next={fetchMoreData}
                 hasMore={hasMore}
-                loader={<div className="loader" style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '9999' }}>Loading...<SyncLoader/></div>}
+                // loader={<div className="loader" style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '9999' }}>Loading...<SyncLoader/></div>}
                 endMessage={<p style={{ textAlign: 'center' }}>You have seen it all</p>}
                 className={CompanyDressListCSS.container}
             >
+                <h1 className={CompanyDressListCSS.h1}>My Dress List</h1>
                 {currentItems.map((dressData, index) => {
                     // 각 드레스 카드에 대한 이미지 URL을 생성합니다.
                     const imageUrl = `http://1.214.19.22:6900/${dressData.dressImagePath}`;
 
                     return (
+                        <div className={CompanyDressListCSS.dressList}>
                         <div key={dressData.dressInfoIndex} className={CompanyDressListCSS.card}>
                             <img src={imageUrl} alt={`Dress ${dressData.dressInfoIndex}`} className={CompanyDressListCSS.image} />
                             <div className={CompanyDressListCSS.content}>
@@ -143,14 +252,18 @@ function  CompanyDressList() {
                                     <div className={CompanyMypageCSS.b1}><b>Dress Name:<br/> {dressData.dressName}</b></div>
                                     <div className={CompanyMypageCSS.b2}><b>markCount: {dressData.markCount}</b></div>
                                     <div className={CompanyMypageCSS.b3}><b>DressInfoIndex: {dressData.dressInfoIndex}</b></div>
-                                    <button onClick={() => handleDelete(index)}>삭제</button>
+                                    <button className={CompanyDressListCSS.delete} onClick={() => handleDelete(index)}>삭제</button>
                                 </div>
                             </div>
                         </div>
+                        </div>
                     );
                 })}
-            </InfiniteScroll>
 
+            </InfiniteScroll>
+            </div>
+
+            <Footer />
         </div>
     );
 
--- react/src/pages/company/CompanyDressRe.js
+++ react/src/pages/company/CompanyDressRe.js
@@ -1,7 +1,11 @@
 import CompanyDressReCSS from './CompanyDressRe.module.css'
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import qs from 'qs';
+import Footer from "../../components/common/Footer";
+import CompanyMypageCSS from "./CompanyMypage.module.css";
+import {Icon} from "@iconify/react";
+import Swal from "sweetalert2";
 function  CompanyDressRe() {
 
     /* 드레스 등록하기 */
@@ -13,12 +17,71 @@ function  CompanyDressRe() {
     const [designer, setDesigner] = useState('');
     const [dressPrice, setDressPrice] = useState('');
     const [dressExplanation, setDressExplanation] = useState('');
+    const [companyName, setCompanyName] = useState('');
 
     const handlerChangeDressName = e => setDressName(e.target.value);
     const handlerChangeDressPNumber = e => setDressPNumber(e.target.value);
     const handlerChangeDesigner = e => setDesigner(e.target.value);
     const handlerChangeDressPrice = e => setDressPrice(e.target.value);
     const handlerChangeDressExplanation = e => setDressExplanation(e.target.value);
+
+    const showAlert = () => {
+        if (!accessToken) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "유효한 토큰값이 없습니다.",
+                showConfirmButton: false,
+                timer: 3000,
+            }).then(() => {
+                // 3초 후에 실행
+                setTimeout(() => {
+                    window.location.href = '/login'; // '/login'은 로그인 페이지 경로에 따라 수정
+                });
+            });
+        } else {
+
+        }
+    };
+
+
+    useEffect(() => {
+        showAlert()
+    }, []);
+
+
+    const info = () => {
+        axios.post(
+            'http://1.214.19.22:6900/com/mypage',
+            {},
+            {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            }
+        ).then((response) => {
+            // HTTP 상태 코드가 200일 때 응답 데이터에서 정보를 추출합니다.
+            if (response.status === 200) {
+                const {
+                    companyName,
+
+                } = response.data;
+
+                // Setting company information
+                setCompanyName(companyName);
+
+            }
+
+        }).catch((error) => {
+            // 에러 핸들링
+        });
+    };
+
+    useEffect(() => {
+        info()
+    }, []);
 
     const [front, setFront] = useState(null);
     const [previewUrl, setPreviewUrl] = useState('');
@@ -28,37 +91,33 @@ function  CompanyDressRe() {
         if (fileF) {
             setFront(fileF);
             setPreviewUrl(URL.createObjectURL(fileF));
-            console.log(fileF);
         }
     };
 
     const [side, setSide] = useState(null);
-    const [previewUrlf, setPreviewUrlF] = useState('');
+    const [previewUrlS, setPreviewUrlS] = useState('');
 
     const handleImageChangeSide = (e) => {
         const fileS = e.target.files[0];
         if (fileS) {
             setSide(fileS);
-            setPreviewUrlF(URL.createObjectURL(fileS));
-            console.log(fileS);
+            setPreviewUrlS(URL.createObjectURL(fileS));
         }
     };
 
     const [back, setBack] = useState(null);
-    const [previewUrls, setPreviewUrlS] = useState('');
+    const [previewUrlB, setPreviewUrlB] = useState('');
 
     const handleImageChangeBack = (e) => {
         const fileB = e.target.files[0];
         if (fileB) {
             setBack(fileB);
-            setPreviewUrlS(URL.createObjectURL(fileB));
-            console.log(fileB);
+            setPreviewUrlB(URL.createObjectURL(fileB));
         }
     };
 
     // 등록
     const onChangeUpload = () => {
-
         const formData = new FormData();
         formData.append('front', front);
         formData.append('side', side);
@@ -81,19 +140,49 @@ function  CompanyDressRe() {
         )
             .then(response => {
                 // 성공적으로 서버 응답을 받았을 때 수행할 작업
-                console.log("Successfully uploaded the dress:", response.data);
+                Swal.fire({
+                    icon: "success",
+                    title: "업로드 성공",
+                    text: "드레스가 성공적으로 업로드되었습니다.",
+                });
+                // 성공 후 추가 작업 수행 가능
             })
             .catch(error => {
                 // 오류 처리
-                console.error("Error uploading dress:", error);
+                Swal.fire({
+                    icon: "error",
+                    title: "업로드 실패",
+                    text: "드레스 업로드 중 오류가 발생했습니다.",
+                });
+            });
+    };
 
-                // 에러 상세 정보 로깅
-                if (error.response) {
-                    console.error("Server responded with:", error.response.data);
-                } else if (error.request) {
-                    console.error("No response received from server.");
+    const handleCompanyNameChange = (e) => {
+        setCompanyName(e.target.value);
+    };
+
+    const handleLogout = () => {
+        Swal.fire({
+            title: "로그아웃 하시겠습니까?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes!",
+            cancelButtonText: "No!",
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        "메인화면으로 돌아갑니다.",
+                        "로그아웃에 성공하였습니다.",
+                        "success"
+                    );
+                    // 로그아웃 처리 및 기타 작업 수행
+                    sessionStorage.clear();
+                    window.location.href = '/'; // 메인 페이지로 이동
                 } else {
-                    console.error("Error setting up the request:", error.message);
+                    Swal.fire("취소되었습니다.", "Your imaginary file is safe :)", "info");
                 }
             });
     };
@@ -101,70 +190,84 @@ function  CompanyDressRe() {
 
         return(
         <div className={CompanyDressReCSS.backgroundDiv}>
+            <div className={CompanyDressReCSS.companyDiv}>
+                <input type='text' className={CompanyDressReCSS.name} value={companyName} onChange={handleCompanyNameChange}/> <span className={CompanyDressReCSS.a}>님 환영합니다.</span>
+                <button className={CompanyDressReCSS.logout} onClick={handleLogout}>로그아웃</button>
+                <Icon className={CompanyDressReCSS.icon_logout} icon="line-md:logout" />
+            </div>
             {/* 이미지 등록 */}
             <div className={CompanyDressReCSS.regsterDiv}>
-                <h1>이미지 등록 </h1>
+                <h1 className={CompanyDressReCSS.h1}>Dress Registration</h1>
+                <h2 className={CompanyDressReCSS.h2}>Image</h2>
+                <div className={CompanyDressReCSS.imagebox}>
                 <div className={CompanyDressReCSS.frontDiv}>
-                    <li> 드레스 앞 이미지</li>
+                    <li className={CompanyDressReCSS.li1}>Front</li>
+                    <div className={CompanyDressReCSS.front}>
+                    {/* 이미지 미리보기 */}
+                    <img alt="front" src={previewUrl} key={previewUrl} className={CompanyDressReCSS.imgfront} ></img>
+                    </div>
                     {/* 이미지 업로드 */}
                     <input type="file" id="fileF" accept="image/*"
-                           style={{ border: "solid 1px lightgray", borderRadius: "5px" }}
+                           className={CompanyDressReCSS.nameInputimage}
                            onChange={handleImageChange} />
 
-                    {/* 이미지 미리보기 */}
-                    <img alt="front" src={front} style={{ maxWidth: "100px" }}></img>
                 </div>
                 <div className={CompanyDressReCSS.sideDiv}>
-                    <li>드레스 옆 이미지</li>
+                    <li className={CompanyDressReCSS.li2}>Side</li>
+                    <div className={CompanyDressReCSS.side}>
+                        {/* 이미지 미리보기 */}
+                    <img alt="side" src={previewUrlS} key={previewUrlS} className={CompanyDressReCSS.imgside}></img>
+                </div>
                     {/* 이미지 업로드 */}
                     <input type="file" id="fileS" accept="image/*"
-                           style={{ border: "solid 1px lightgray", borderRadius: "5px" }}
+                           className={CompanyDressReCSS.nameInputimages}
                            onChange={handleImageChangeSide} />
-
-                    {/* 이미지 미리보기 */}
-                    <img alt="side" src={side} style={{ maxWidth: "100px" }}></img>
                 </div>
                 <div className={CompanyDressReCSS.backDiv}>
-                    <li>드레스 뒤 이미지</li>
+                    <li className={CompanyDressReCSS.li3}>Back</li>
+                    <div className={CompanyDressReCSS.back}>
+                        {/* 이미지 미리보기 */}
+                        <img alt="back" src={previewUrlB} key={previewUrlB} className={CompanyDressReCSS.imgback}></img>
+                    </div>
                     {/* 이미지 업로드 */}
                     <input type="file" id="fileB" accept="image/*"
-                           style={{ border: "solid 1px lightgray", borderRadius: "5px" }}
+                           className={CompanyDressReCSS.nameInputimageb}
                            onChange={handleImageChangeBack} />
-
-                    {/* 이미지 미리보기 */}
-                    <img alt="back" src={back} style={{ maxWidth: "100px" }}></img>
                 </div>
+            </div>
             </div>
             
             {/* 드레스 정보 등록 */}
+            <h2 className={CompanyDressReCSS.h2}>Dress Info</h2>
             <div className={CompanyDressReCSS.infoDiv}>
                 <div className={CompanyDressReCSS.nameDiv}>
-                    드레스명 :
-                    <input type="text" value={dressName} onChange={handlerChangeDressName}/>
+                    Dress Name :
+                    <input type="text" className={CompanyDressReCSS.nameinput} value={dressName} onChange={handlerChangeDressName}/>
                 </div>
                 <div className={CompanyDressReCSS.numberDiv}>
-                    드레스 품번 :
-                    <input type="text" value={dressPNumber} onChange={handlerChangeDressPNumber}/>
+                    Dress Number :
+                    <input type="text" className={CompanyDressReCSS.numberinput} value={dressPNumber} onChange={handlerChangeDressPNumber}/>
                 </div>
                 <div className={CompanyDressReCSS.designerDiv}>
-                    디자이너 :
-                    <input type="text" value={designer} onChange={handlerChangeDesigner}/>
+                    Designer :
+                    <input type="text" className={CompanyDressReCSS.designerinput} value={designer} onChange={handlerChangeDesigner}/>
                 </div>
                 <div className={CompanyDressReCSS.priceDiv}>
-                    가격 :
-                    <input type="text" value={dressPrice} onChange={handlerChangeDressPrice}/>
+                    Price :
+                    <input type="text" className={CompanyDressReCSS.priceinput} value={dressPrice} onChange={handlerChangeDressPrice}/>
                 </div>
             </div>
             {/* 상세 설명 등록 */}
             <div className={CompanyDressReCSS.detailDiv}>
-                상세 설명 등록
-                <input type="text" value={dressExplanation} onChange={handlerChangeDressExplanation}/>
+                <h2 className={CompanyDressReCSS.h2}>Detail</h2>
+                <input type="text" className={CompanyDressReCSS.inputDetail} value={dressExplanation} onChange={handlerChangeDressExplanation}/>
             </div>
             
             {/* 등록하기*/}
             <div className={CompanyDressReCSS.btnDiv}>
-                <button onClick={onChangeUpload}>등록하기</button>
+                <button className={CompanyDressReCSS.upload} onClick={onChangeUpload}>등록하기</button>
             </div>
+            <Footer/>
         </div>
     );
 
